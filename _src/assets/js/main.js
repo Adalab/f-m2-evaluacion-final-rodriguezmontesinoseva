ee6abee5e7368fb9b20e028c47ac0a4b3efdd258
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-22 23:51:58 +0200

    add elements array favourites

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,12 +4,16 @@ const inpSearchEl = document.querySelector('#inpSearch');
 const btnSearchEl = document.querySelector('.btnSearch');
 const listUlEl = document.querySelector('.listUl');
 
+const listUlFavEl= document.querySelector('.listUlFav');
+
+
+
+
 
 
 //hace busqueda al servidor y pinta nombre e imagen de series que coinciden con la búsqueda
 function handleSearch(){
-  const arrResult = [];
-
+const arrResult =[];
   fetch(`http://api.tvmaze.com/search/shows?q=${inpSearchEl.value}`)
     .then(response => response.json())
     .then(data => {
@@ -28,12 +32,16 @@ function handleSearch(){
         arrResult.push({name: nameS, image: imageS,});
 
         const listContent = `<li>${nameS}<img src="${imageS}" /> </li>`;
+
         ulContent += listContent;
         listUlEl.innerHTML = ulContent;
-        console.log(listUlEl);// hay que hacer un return listUlEl y trabajar sobre esa lista
-        changeBgc(listUlEl);
+
+       // console.log('>>>>> ', listUlEl);// hay que hacer un return listUlEl y trabajar sobre esa lista
+        // changeBgc(listUlEl);
       }
 
+      const liEl = document.querySelectorAll('.listUl li');
+      liEl.forEach(element => element.addEventListener('click', changeBgc));
 
     });
 
@@ -41,10 +49,9 @@ function handleSearch(){
 
 btnSearchEl.addEventListener('click', handleSearch);
 
-const liEl = document.querySelectorAll('li');
 
-liEl.forEach(element => element.addEventListener('click', changeBgc));
-const arrFav=[];
+
+// liEl.forEach(element => element.addEventListener('click', changeBgc));
 
 // function pintaParrCache(event) {
 //     if (event.target.id === "inName") {
@@ -54,10 +61,39 @@ const arrFav=[];
 //     }
 
 function changeBgc(event){
-//   console.log(event);
-//   const liEl = document.querySelectorAll('li');
-//   console.log(liEl);
-//   liEl.classlist.add('inv-colors');
-//   arrFav.push({name: nameS, image: imageS,});
+
+
+  event.currentTarget.classList.toggle('inv-colors');
+  fillFav();
+
+
+
+//console.log(arrFav);
+
+
+  //const listContentFav = `<li>${nameF}<img src="${imageF}" /> </li>`;
+
+
+ // ulContent += listContentFav;
+  
+ //listUlFavEl.innerHTML = listUlFavEl.innerHTML + listContentFav;
+
+
 //   // pintar el array y colocarlo a la izda. si no existe h2 lo crea
+}
+
+function fillFav(){
+  const liEl = document.querySelectorAll('.listUl li');
+  const arrFav=[];
+
+console.log(liEl);
+
+  for(let i= 0; i< liEl.length; i++){
+     if(liEl[i].classList.contains('inv-colors')) {
+       const nameF = liEl[i].textContent;
+       const imageF = liEl[i].querySelector('img').src;
+        arrFav.push({name: nameF, image: imageF});
+       };
+  }
+  console.log(arrFav);
 }
