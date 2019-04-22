692b1205a3ef7d91680a8b08984e2a84147c5fca
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-22 20:27:31 +0200

    try event favourites

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,64 +1,63 @@
 'use strict';
 
-// console.log('>> Ready :)');
-
 const inpSearchEl = document.querySelector('#inpSearch');
 const btnSearchEl = document.querySelector('.btnSearch');
 const listUlEl = document.querySelector('.listUl');
 
 
-function handleSearch(){
-    const arrResult = [];
 
-    fetch(`http://api.tvmaze.com/search/shows?q=${inpSearchEl.value}`)
+//hace busqueda al servidor y pinta nombre e imagen de series que coinciden con la búsqueda
+function handleSearch(){
+  const arrResult = [];
+
+  fetch(`http://api.tvmaze.com/search/shows?q=${inpSearchEl.value}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        let ulContent = "";
+      let ulContent = '';
 
-        for (let i=0; i<data.length; i++){
-            const nameS = data[i].show.name;
-            let imageS;
-            if (data[i].show.image === null){
-                  imageS = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+      for (let i=0; i<data.length; i++){
+        const nameS = data[i].show.name;
+        let imageS;
+        if (data[i].show.image === null){
+          imageS = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 
-            }else{
-              
-                imageS = data[i].show.image.medium;
-            };
-            console.log ("nameS"+[i] + "es "+ nameS);
-            console.log ("imageS"+[i] + " es "+ imageS);
-            arrResult.push({name: nameS, image: imageS,},);
-            console.log(arrResult);
+        }else{
 
-            const listContent = `<li>${nameS}<img src="${imageS}" /> </li>`;
-            ulContent += listContent;
-            listUlEl.innerHTML = ulContent;
-
+          imageS = data[i].show.image.medium;
         }
+        arrResult.push({name: nameS, image: imageS,});
 
-//         const reposContent = `<li>${repos}</li>`;
-// //     ulContent += reposContent;
-// ul.innerHTML = ulContent;
+        const listContent = `<li>${nameS}<img src="${imageS}" /> </li>`;
+        ulContent += listContent;
+        listUlEl.innerHTML = ulContent;
+        console.log(listUlEl);// hay que hacer un return listUlEl y trabajar sobre esa lista
+        changeBgc(listUlEl);
+      }
+
+
     });
 
 }
 
-// fetch(`https://api.github.com/orgs/${input}`) //ponerlo entre acentos graves
-// .then(response => response.json())
-// .then(data => {
-//   console.log('Breeds data response: ', data);
-
-//   const ul = document.querySelector('ul');
-//   const organGit = data.message;
-//   let ulContent = '';
-
-//   for (const repos of organGit) {
-//     const reposContent = `<li>${repos}</li>`;
-//     ulContent += reposContent;
-//   }
-//   ul.innerHTML = ulContent;
-// });
-
-
 btnSearchEl.addEventListener('click', handleSearch);
+
+const liEl = document.querySelectorAll('li');
+
+liEl.forEach(element => element.addEventListener('click', changeBgc));
+const arrFav=[];
+
+// function pintaParrCache(event) {
+//     if (event.target.id === "inName") {
+//       parrNameEl.innerHTML = event.target.value;
+//     } else if (event.target.id === "inSurname") {
+//       parrSurEl.innerHTML = event.target.value;
+//     }
+
+function changeBgc(event){
+//   console.log(event);
+//   const liEl = document.querySelectorAll('li');
+//   console.log(liEl);
+//   liEl.classlist.add('inv-colors');
+//   arrFav.push({name: nameS, image: imageS,});
+//   // pintar el array y colocarlo a la izda. si no existe h2 lo crea
+}
