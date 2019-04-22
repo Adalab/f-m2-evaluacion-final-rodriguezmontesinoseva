115f5d561fd98bca083b23bc8a7eaefa84a65fba
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-22 18:47:38 +0200

    build array

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,7 +10,31 @@ const listUlEl = document.querySelector('.listUl');
 function handleSearch(){
     const arrResult = [];
 
-    fetch
+    fetch(`http://api.tvmaze.com/search/shows?q=${inpSearchEl.value}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let ulContent;
+
+        for (let i=0; i<data.length; i++){
+            const nameS = data[i].show.name;
+            if (data[i].show.image !== null){
+                const imageS = data[i].show.image.medium;
+
+            }else{
+                const imageS = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+            }
+            arrResult.push({name: nameS, image: imageS});
+            const listContent = `<li>${repos}</li>`;
+            ulContent += listContent;
+            listUlEl.innerHTML = ulContent;
+
+        }
+
+//         const reposContent = `<li>${repos}</li>`;
+// //     ulContent += reposContent;
+// ul.innerHTML = ulContent;
+    });
 
 }
 
