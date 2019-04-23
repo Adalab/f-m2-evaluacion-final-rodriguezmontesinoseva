b1cc25ac1ac24a38603f649d671046aa82b14f40
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-23 02:24:07 +0200

    add local storage add delete il

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,15 +5,14 @@ const btnSearchEl = document.querySelector('.btnSearch');
 const listUlEl = document.querySelector('.listUl');
 
 const listUlFavEl= document.querySelector('.listUlFav');
+let arrFav = localStorage.getItem('arrFav') ? JSON.parse( localStorage.getItem('arrFav')) : [];
 
-
-
-
-
+btnSearchEl.addEventListener('click', handleSearch);
+drawFav();
 
 //hace busqueda al servidor y pinta nombre e imagen de series que coinciden con la búsqueda
 function handleSearch(){
-const arrResult =[];
+  const arrResult =[];
   fetch(`http://api.tvmaze.com/search/shows?q=${inpSearchEl.value}`)
     .then(response => response.json())
     .then(data => {
@@ -35,65 +34,63 @@ const arrResult =[];
 
         ulContent += listContent;
         listUlEl.innerHTML = ulContent;
-
-       // console.log('>>>>> ', listUlEl);// hay que hacer un return listUlEl y trabajar sobre esa lista
-        // changeBgc(listUlEl);
       }
 
       const liEl = document.querySelectorAll('.listUl li');
       liEl.forEach(element => element.addEventListener('click', changeBgc));
-
     });
-
 }
-
-btnSearchEl.addEventListener('click', handleSearch);
-
-
-
-// liEl.forEach(element => element.addEventListener('click', changeBgc));
-
-// function pintaParrCache(event) {
-//     if (event.target.id === "inName") {
-//       parrNameEl.innerHTML = event.target.value;
-//     } else if (event.target.id === "inSurname") {
-//       parrSurEl.innerHTML = event.target.value;
-//     }
 
 function changeBgc(event){
-
+  const nameF = event.currentTarget.textContent;
+  const imageF = event.currentTarget.querySelector('img').src;
+  listUlFavEl.innerHTML = '';
 
   event.currentTarget.classList.toggle('inv-colors');
-  fillFav();
+  //si lo selecciono como favorito, comprobamos si existe en arr Favoritos
+  if(event.currentTarget.classList.contains('inv-colors')) {
+    if (arrFav.length > 0) {
+      //si el objeto que devuelve find es === undefined es que no encuentra nada. => si el elemento clicado no existe en arrayFav pushea
+      if(arrFav.find(element => element.name === nameF && element.image === imageF) === undefined){
+        arrFav.push({name: nameF, image: imageF});
+      }
+    } else {
+      arrFav.push({name: nameF, image: imageF});
+    }
+  } else { //si no contiene inv-colors. crea nuevo array sin el elemento clicado
+    arrFav = arrFav.filter(element => element.name !== nameF && element.image !== imageF);
+  }
 
-
-
-//console.log(arrFav);
-
-
-  //const listContentFav = `<li>${nameF}<img src="${imageF}" /> </li>`;
-
-
- // ulContent += listContentFav;
-  
- //listUlFavEl.innerHTML = listUlFavEl.innerHTML + listContentFav;
-
-
-//   // pintar el array y colocarlo a la izda. si no existe h2 lo crea
+  drawFav();
+  localStorage.setItem('arrFav', JSON.stringify(arrFav) );
 }
 
-function fillFav(){
-  const liEl = document.querySelectorAll('.listUl li');
-  const arrFav=[];
+function drawFav() {
+  const subtitleEl = document.querySelector('.subtitle');
+  listUlFavEl.innerHTML = '';
 
-console.log(liEl);
-
-  for(let i= 0; i< liEl.length; i++){
-     if(liEl[i].classList.contains('inv-colors')) {
-       const nameF = liEl[i].textContent;
-       const imageF = liEl[i].querySelector('img').src;
-        arrFav.push({name: nameF, image: imageF});
-       };
+  // lee el array Fav y lo pinta en el ul
+  if(arrFav.length > 0) {
+    for(let i=0; i<arrFav.length;i++){
+      const listContentFav = `<li>${arrFav[i].name}<img src="${arrFav[i].image}" /><a class="delete" href="#">borrar x</a></li>`;
+      listUlFavEl.innerHTML = listUlFavEl.innerHTML + listContentFav;
+    }
+    document.querySelectorAll('.delete').forEach(element =>
+      element.addEventListener('click', deleteFav)
+    );
+    subtitleEl.innerHTML = 'Mis series favoritas';
+  } else {
+    subtitleEl.innerHTML = '';
   }
-  console.log(arrFav);
+}
+
+function deleteFav(event) {
+  const nameF = event.target.parentElement.textContent;
+  const imageF = event.target.parentElement.querySelector('img').src;
+
+//carga en arrFav todo lo que no se haya clicado
+  arrFav = arrFav.filter(element => element.name !== nameF && element.image !== imageF);
+
+  drawFav();
+  localStorage.setItem('arrFav', JSON.stringify(arrFav) );
 }
